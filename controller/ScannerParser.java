ab13f453cc9870b81d8e90177186d480fff6def8
Yuki Enomoto <yuki_enomot0@yahoo.com>
2016-04-03 23:17:19 +0800

    With view (Parse Tree)

--- controller/ScannerParser.java
+++ controller/ScannerParser.java
@@ -1,10 +1,18 @@
 package controller;
 
 import java.io.IOException;
+import java.util.Arrays;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.ScrollPaneConstants;
 
 import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.gui.TreeViewer;
 
 import antlr.CustomErrorListener;
 import antlr.GrammarBaseListener;
@@ -16,7 +24,7 @@ public class ScannerParser {
 		 GrammarLexer lexer = null;
 		 CustomErrorListener customErrorListener = null;
 	        try {
-	        	ANTLRFileStream input = new ANTLRFileStream("hello.txt");
+	        	ANTLRFileStream input = new ANTLRFileStream("Hello.txt");
 	        	
 	        	customErrorListener = new CustomErrorListener();
 	        	
@@ -51,5 +59,27 @@ public class ScannerParser {
 	        		e.printStackTrace();
 	        	}
 	        }
+	        
+	        //show AST in console
+	        //System.out.println(tree.toStringTree(p));
+
+	        //show AST in GUI
+	        JFrame frame = new JFrame("Antlr AST");
+	        JPanel panel = new JPanel();
+	        
+	        TreeViewer viewr = new TreeViewer(Arrays.asList(p.getRuleNames()),tree);
+	        viewr.setScale(1.5);//scale a little
+	        panel.add(viewr);
+	        frame.add(panel);
+	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	        frame.setResizable(true);
+	        frame.setSize(frame.getMaximumSize());
+	        frame.setVisible(true);
+	        
+	        final JTextArea display = new JTextArea(16, 58);
+	        display.setEditable(true);
+	        JScrollPane scroll = new JScrollPane(display);
+	        scroll.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+	        panel.add(scroll);
 	}
 }
